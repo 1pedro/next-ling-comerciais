5295016b687efe45657cbadac01c978d4e4ea1f7
Pedro Jefferson <pedrojefferson.dev@gmail.com>
2023-09-27 21:52:29 -0300

    start frontend

---
+++ app/pages/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Image from "next/image";
+
+import { Dancing_Script, Inclusive_Sans } from 'next/font/google'
+
+const font = Dancing_Script({ subsets: ['latin'] })
+
+
+function Header(props) {
+    return (
+        <div className={`w-full p-5 text-center text-white font-bold bg-red-600 text-5xl h-[120px] flex justify-between ${font.className}`}>
+            <img
+                src="/pizza.png"
+                alt="Pizza"
+                style={{ width: 110}}
+            />
+            <h1>Sistemas de Pedidos</h1>
+            <h2>+ 71 123 456 789</h2>
+        </div>
+    );
+}
+
+export default Header;
