4cca544e8d48392949150e257b25b58066ac1d76
Pedro Jefferson <pedrojefferson.dev@gmail.com>
2023-09-27 22:02:41 -0300

    fixes

--- app/pages/Footer.tsx
+++ app/pages/Footer.tsx
@@ -5,13 +5,13 @@ import { Dancing_Script, Inclusive_Sans } from 'next/font/google'
 const font = Dancing_Script({ subsets: ['latin'] })
 const textFont = Inclusive_Sans({weight: "400", subsets: ['latin']})
 
-function Footer(props) {
+function Footer(props:any ) {
     return (
         <div className={'w-full p-10 text-white bg-zinc-600  h-[300px] flex flex-col'}>
             <h2 className={`text-4xl ${font.className} `}>Sistemas de Pedidos</h2>
             <p className={`text-md ${textFont.className}`}>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.  <br/>
+                Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.  <br/>
                 It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
                 <br/>
                 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
--- app/pages/Header.tsx
+++ app/pages/Header.tsx
@@ -6,12 +6,13 @@ import { Dancing_Script, Inclusive_Sans } from 'next/font/google'
 const font = Dancing_Script({ subsets: ['latin'] })
 
 
-function Header(props) {
+function Header(props: any) {
     return (
         <div className={`w-full p-5 text-center text-white font-bold bg-red-600 text-5xl h-[120px] flex justify-between ${font.className}`}>
-            <img
+            <Image
                 src="/pizza.png"
                 alt="Pizza"
+                width={110}
                 style={{ width: 110}}
             />
             <h1>Sistemas de Pedidos</h1>
