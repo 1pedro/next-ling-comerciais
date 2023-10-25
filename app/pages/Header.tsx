19d12de8731cff7be63a96923caf11d917744f26
Pedro Jefferson <pedrojefferson.dev@gmail.com>
2023-10-24 21:07:16 -0300

    fix

--- app/pages/Header.tsx
+++ app/pages/Header.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Image from "next/image";
-import font from 
 
 import { Dancing_Script, Inclusive_Sans } from 'next/font/google'
 
@@ -10,11 +9,12 @@ const font = Dancing_Script({ subsets: ['latin'] })
 function Header(props: any) {
     return (
         <div className={`w-full p-5 text-center text-white font-bold bg-red-600 text-5xl h-[120px] flex justify-between ${font.className}`}>
-            <Image
+            <img
                 src="/pizza.png"
                 alt="Pizza"
                 width={110}
-                style={{ width: 110}}
+                height={110}
+                style={{ width: 110, height: 110}}
             />
             <h1>FoodStips</h1>
             <ul>
