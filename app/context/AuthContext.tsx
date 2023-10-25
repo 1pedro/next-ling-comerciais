c2f5c06ca47f05f6320264c562a6e4ce58a03c1e
Pedro Jefferson <pedrojefferson.dev@gmail.com>
2023-10-25 17:33:40 -0300

    Add firebase

---
+++ app/context/AuthContext.tsx
@@ -0,0 +1,34 @@
+import React, {ReactElement} from 'react';
+import {getAuth, onAuthStateChanged, User as FirebaseUser} from 'firebase/auth';
+import firebase_app from '../firebase/config';
+
+const auth = getAuth(firebase_app);
+
+export const AuthContext = React.createContext({});
+
+export const useAuthContext = () => React.useContext(AuthContext);
+
+export const AuthContextProvider = ({children}: { children: ReactElement }) => {
+    const [user, setUser] = React.useState<FirebaseUser | null>(null);
+    const [loading, setLoading] = React.useState(true);
+
+
+    React.useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{user}}>
+            {loading ? <div>Loading...</div> : children}
+        </AuthContext.Provider>
+    );
+};
